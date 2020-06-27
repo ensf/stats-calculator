83f9d86b0f91c7ece5baf6f12867d96c0088c511
Stan Chen <stanleychen587@gmail.com>
2020-06-27 08:55:17 -0600

    implemented basic sql wrappers: createDB, queryDB, insert, selectFrom

--- src/main/java/DataFrame/SQLite.java
+++ src/main/java/DataFrame/SQLite.java
@@ -1,23 +1,95 @@
 package DataFrame;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLite {
     /**
-     * Initiate an empty SQLite file
-     * @param fn database name for the created database
+     * Note: can work with any schema
+     * Set up a new connection based on incoming parameter fn
+     * @param db DB name to connect
+     * @return a Connection to specified db
      */
-    public static void createNewDB(String fn) {
-        String url = "jdbc:sqlite:" + fn;
-        try (Connection connection = DriverManager.getConnection(url)) {
+    public static Connection connDB(String db) {
+        String url = "jdbc:sqlite:" + db;
+        Connection dbConnection = null;
+        try {
+            dbConnection = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return dbConnection;
+    }
+
+    /**
+     * Note: can work with any schema
+     * Initiate an empty SQLite file
+     * @param db database name for the created database
+     */
+    public static void createNewDB(String db) {
+        try (Connection connection = connDB(db))
+        {
             if (connection != null) {
                 DatabaseMetaData metaData = connection.getMetaData();
-                System.out.println("Database: \""+fn+"\" has been Created. \nDB Driver: " + metaData.getDriverName());
+                System.out.println("Database: \""+db+"\" has been Created. \nDB Driver: " + metaData.getDriverName());
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
+
+    /**
+     * Note: can work with any schema
+     * @param db
+     * @param sql
+     */
+    public static void queryDB(String db, String sql) {
+        try (Connection conn = connDB(db);
+             Statement statement = conn.createStatement()) {
+            statement.execute(sql);
+            System.out.println("The following query was executed:\n---\n"+sql+"\n---");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Note: currently schema specific, need to make it universal
+     * @param db
+     * @param tb
+     * @param colNames
+     * @param values
+     */
+    public static void insert(String db,String tb,String colNames, String values) {
+        String sql="INSERT INTO "+tb+"("+colNames+") VALUES ("+values+");";
+        try (Connection conn = connDB(db);
+             Statement statement = conn.createStatement()) {
+            statement.execute(sql);
+            System.out.println("The following query was executed:\n"+sql+"\n---");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Wrapper for SQL select from
+     * @param db database name
+     * @param tb table name
+     * @param colNames columns to select from
+     */
+    public static void selectFrom(String db, String tb, String colNames){
+        String sql = "SELECT "+colNames+" FROM "+tb;
+
+        try (Connection conn = connDB(db);
+             Statement statement  = conn.createStatement();
+             ResultSet resultSet    = statement.executeQuery(sql)){
+            // loop through the result set
+            while (resultSet.next()) {
+                System.out.println(//resultSet.getInt("id") +  "\t" +
+                        resultSet.getString("name") + "\t" +
+                        resultSet.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
--- src/test/java/StatsCalculator/SQLiteTest.java
+++ src/test/java/StatsCalculator/SQLiteTest.java
@@ -1,11 +1,23 @@
 package StatsCalculator;
-import static DataFrame.SQLite.createNewDB;
+import static DataFrame.SQLite.*;
 
 public class SQLiteTest {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        createNewDB("newDB.db");
+        createNewDB("test.db");
+        String sql = "CREATE TABLE IF NOT EXISTS DataFrame (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+        queryDB("test.db",sql);
+        insert("test.db","DataFrame","name, capacity","\'jack hammer\', 50.00");
+        insert("test.db","DataFrame","name, capacity","\'drill bit\', 15.00");
+        insert("test.db","DataFrame","name, capacity","\'2x4 block\', 5.00");
+        insert("test.db","DataFrame","name, capacity","\'power washer\', 120.00");
+        selectFrom("test.db","DataFrame","name, capacity");
     }
+
 }
