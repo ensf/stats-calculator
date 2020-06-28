17cdf33a34b1bdf08eae0a37df02d97b287c5721
Stan Chen <stanleychen587@gmail.com>
2020-06-28 11:08:05 -0600

    implemented #1:version label and url to be displayed on cli #1

---
+++ src/test/java/StatsCalculator/StatsCalculatorAppTest.java
@@ -0,0 +1,13 @@
+package StatsCalculator;
+
+import static StatsCalculator.StatsCalculatorApp.versionLabel;
+
+public class StatsCalculatorAppTest {
+    /**
+     * Implemented test for version label method.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(versionLabel());
+    }
+}
